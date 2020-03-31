c2de9bf41828efbfffd3ab291d66a0a6ff70b27f
indy33817719 <pppike.0131@gmail.com>
2020-03-31 21:50:28 +0900

    revise provider

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import reducer from './reducers';
-
-import App from '.components/App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import serviceWorker from './serviceWorker';
 
 // App内のstateをこのstoreに集約
 const store = createStore(reducer);
 
 
 ReactDOM.render(
-    // Providerを用いてApp内の全componentをstoreに集約
-    <Provider store={store}>
+    <Provider store={ store }>
         <App/>
-    </Provider>
-  document.getElementById('root')
-);
+    </Provider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
