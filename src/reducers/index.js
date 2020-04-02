19363344d46f6cfb1e48ef672ba7a4ca1f9057a5
indy33817719 <pppike.0131@gmail.com>
2020-04-02 23:45:58 +0900

    implement event new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 // app内に存在するreducerを総括する、結合する役割
 
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
 // store作成時に必要
-export default combineReducers({ events })
+export default combineReducers({ events, form})
 
