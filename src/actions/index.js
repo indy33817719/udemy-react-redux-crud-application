0d16d7a0e8594528542f2b853faad95b47004101
indy33817719 <pppike.0131@gmail.com>
2020-04-02 23:03:59 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
+// readEventsが外部のAPIサーバーにリクエストする
+// そのリクエストをするHTTPクライアントをimport
+import axios from 'axios'
 // actionを定義
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+export const READ_EVENTS = 'READ_EVENTS'
+
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
 
 // actionをリターンする関数
 // view側でインポートさせることで然るべき状態遷移をさせる
-export const increment = () => ({
-        type: INCREMENT
-})
-
-export const decrement = () => ({
-        type: DECREMENT
-})
+export const readEvents = () => async dispatch => {
+        const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+        dispatch({ type: READ_EVENTS, response});
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 // app内に存在するreducerを総括する、結合する役割
 
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
 // store作成時に必要
-export default combineReducers({ count })
+export default combineReducers({ events })
 
